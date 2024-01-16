7c6a024c784df0c18663937040f78289b1271cc6
rammsteeein <151989069+rammsteeein@users.noreply.github.com>
2024-01-16 15:25:11 +0300

    Add files via upload

---
+++ map/chess.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="chess" tilewidth="32" tileheight="32" tilecount="80" columns="10">
+ <image source="Tc9r+Y.png" width="347" height="260"/>
+ <tile id="34">
+  <properties>
+   <property name="chess_play" value="1"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="chess_play" value="1"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="chess_play" value="1"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="chess_play" value="1"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ map/stones1.tsx
@@ -0,0 +1,337 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="stones1" tilewidth="32" tileheight="32" tilecount="2448" columns="51">
+ <image source="MainLev2.0.png" width="1632" height="1536"/>
+ <tile id="861">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="862">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="863">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="864">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="865">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="866">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="912">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="913">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="914">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="915">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="916">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="917">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="963">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="964">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="965">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="966">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="967">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="968">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1014">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1015">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1016">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1017">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1018">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1019">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1065">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1066">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1067">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1068">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1069">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1070">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1116">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1117">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1118">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1119">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1120">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1121">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2135">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2136">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2137">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2138">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2139">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2186">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2187">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2188">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2189">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2190">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2237">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2238">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2239">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2240">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2241">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2288">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2289">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2290">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2291">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2292">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2339">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2340">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2341">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2342">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2343">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2390">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2391">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2392">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2393">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2394">
+  <properties>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <wangsets>
+  <wangset name="Безымянный набор" type="corner" tile="-1"/>
+ </wangsets>
+</tileset>
